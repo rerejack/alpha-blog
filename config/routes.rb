b301d76ed71c4f944afcfc73ca9753d195afbccf
Udemy Rails Course <kinchunglaw215@gmail.com>
2017-03-04 16:20:17 +0000

    Root pages/home

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   #  root 'pages#index'
+    root 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
       get 'pages/home',to: 'pages#home'
